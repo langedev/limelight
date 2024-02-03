3e5991f708ef76bb83fae0b4316f9c78ff15cc08
langedev <public@daltonlange.com>
2024-02-02 18:31:42 -0800

    Add ScenePosition innerclass to help with movement
    adds an innerclass to Stage which handles the movement. It provides
    useful functions such as increment and decrement movement, while
    handling all the modular arithmetic in the background.
    
    It is used simply by: First incrementing or decrementing your position
    until your happy with it, and then providing it to scene to be used to
    discover your position.
    
    These functions also return a boolean if they wrap around, which allows
    for actors to check if that happens

--- Scenes/Stage/stage.gd
+++ Scenes/Stage/stage.gd
@@ -4,8 +4,6 @@ class_name Stage extends Node2D
 @export var actor_scene: PackedScene
 @export var tilemap: TileMap
 
-enum StageDir {LEFT = -1, NULL = 0, RIGHT = 1}
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var screensize := get_viewport_rect().size
@@ -19,8 +17,58 @@ func _on_child_entered_tree(node: Node):
 	if node is Actor:
 		node.set_stage(self)
 
-func stage_to_local(pos: int) -> Vector2:
-	return tilemap.map_to_local(Vector2(posmod(pos, position_divisions+2)-1,0))
+func get_stage_position() -> StagePosition:
+	return StagePosition.new(position_divisions+1)
 
-func local_to_stage(local: Vector2) -> int:
-	return posmod(tilemap.local_to_map(local).x+1, position_divisions+2)
+func stage_to_local(pos: StagePosition) -> Vector2:
+	return tilemap.map_to_local(Vector2(pos._get_position(),0))
+
+func get_offstage_right() -> Vector2:
+	return tilemap.map_to_local(Vector2(position_divisions,0))
+
+func get_onstage_right() -> Vector2:
+	return tilemap.map_to_local(Vector2(position_divisions-1,0))
+
+func get_offstage_left() -> Vector2:
+	return tilemap.map_to_local(Vector2(-1,0))
+
+class StagePosition:
+	# 0 is offstage, everything else is onstage
+	var _position := 0
+	var _prev_pos := false
+	var _divisions: int
+
+	func _init(divisions, position = -1):
+		_divisions = divisions
+		_position = position
+
+	# External range is [-1, divisions-2]
+	# Internal range is [0, divisions-1]
+	# This is for compatibility with tilemap
+	func _get_position() -> int:
+		return _position - 1
+
+	# Return true if the last movement call was increment_position otherwise
+	# returns false
+	func previous_movement() -> bool:
+		return _prev_pos
+
+	# Returns true if it wraps divisions-1 -> 0
+	func increment_position() -> bool:
+		_prev_pos = true
+		_position = posmod(_position+1, _divisions)
+		if _position == 0:
+			return true
+		return false
+
+	# Returns true if it wraps 0 -> divisons-1
+	func decrement_position() -> bool:
+		_prev_pos = false
+		_position = posmod(_position-1, _divisions)
+		if _position == (_divisions-1):
+			return true
+		return false
+
+	func force_position(position: int) -> void:
+		_prev_pos = false
+		_position = posmod(position, _divisions)
